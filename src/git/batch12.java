c2cc609d62af2e0c589a695edf74866cd44b0c19
Zackny <zarkocubura@gmail.com>
2022-02-16 20:52:31 -0600

    the commit new

--- src/git/batch12.java
+++ src/git/batch12.java
@@ -5,7 +5,7 @@ public class batch12 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("aaaaa bbbbbb");
-System.out.println("sssssssssssssss");
+System.out.println("sssssssssssssssllllllljjjjjjjjjj");
 	}
 
 }
